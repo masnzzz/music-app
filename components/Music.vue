b0286b745cabbc90f3ba2ec90e451def29a49449
MasanaIto <z3heng0326@gmail.com>
2020-03-14 18:59:13 +0900

    Musicコンポーネントを作成、index.vueで読み込む

---
+++ components/Music.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="main">
+    <button
+        class="startBtn"
+        @click="appStart"
+        v-if="isStarted==false">
+        Start!
+    </button>
+    <section
+        class="touchZone"
+        v-else>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isStarted: false
+    }
+  },
+  methods: {
+    appStart() {
+      this.isStarted = true
+    }
+  }
+}
+</script>
+
+<style>
+.main {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.startBtn {
+  padding: 10px 20px;
+}
+
+.touchZone {
+  background: linear-gradient( 45deg, rgb(255,255,255),rgb(100,255,255) );
+  width: 100%;
+  min-width: 100%;
+  height: 100vh;
+  min-height: 100vh;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,15 @@
 <template>
-  <main class="main">
-    <button
-        class="startBtn"
-        @click="appStart"
-        v-if="isStarted==false">
-        Start!
-    </button>
-    <section
-        class="touchZone"
-        v-else>
-    </section>
-  </main>
+    <no-ssr>
+        <Music />
+    </no-ssr>
 </template>
 
 <script>
+import Music from '~/components/Music'
+
 export default {
-  data() {
-    return {
-      isStarted: false
+    components: {
+        Music
     }
-  },
-  methods: {
-    appStart() {
-      this.isStarted = true
-    }
-  }
 }
 </script>
-
-<style>
-.main {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.startBtn {
-  padding: 10px 20px;
-}
-
-.touchZone {
-  background: linear-gradient( 45deg, rgb(255,255,255),rgb(100,255,255) );
-  width: 100%;
-  min-width: 100%;
-  height: 100vh;
-  min-height: 100vh;
-}
-</style>
