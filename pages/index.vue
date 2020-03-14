e75122ae758561ef94fb7c604bbe983973715a72
MasanaIto <z3heng0326@gmail.com>
2020-03-14 18:50:34 +0900

    スタート画面を作成
    Startボタンを押すとappStartメソッドが動き、文字がStartedになる

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,44 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        music-app
-      </h1>
-      <h2 class="subtitle">
-        My posh Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <main class="main">
+    <button
+        class="startBtn"
+        @click="appStart"
+        v-if="isStarted==false">
+        Start!
+    </button>
+    <section
+        class="touchZone"
+        v-else>
+        Started
+    </section>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      isStarted: false
+    }
+  },
+  methods: {
+    appStart() {
+      this.isStarted = true
+    }
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.main {
+  width: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.startBtn {
+  padding: 10px 20px;
 }
 </style>
