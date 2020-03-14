dd54bedcb9ac8bcbc2a218cdb59812ad57f68663
MasanaIto <z3heng0326@gmail.com>
2020-03-14 18:54:55 +0900

    スタート後の画面を実装
    Startedの文字を削除し、画面全体に水色のグラデーションをつける

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,6 @@
     <section
         class="touchZone"
         v-else>
-        Started
     </section>
   </main>
 </template>
@@ -40,5 +39,13 @@ export default {
 
 .startBtn {
   padding: 10px 20px;
+}
+
+.touchZone {
+  background: linear-gradient( 45deg, rgb(255,255,255),rgb(100,255,255) );
+  width: 100%;
+  min-width: 100%;
+  height: 100vh;
+  min-height: 100vh;
 }
 </style>
